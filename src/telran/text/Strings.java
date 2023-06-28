0c9c2f188a2c3139704b2a9a928ad4184165c773
Yuragranovsky <yuragranovsky@gmail.com>
2023-06-28 21:26:42 +0300

    CW #25 compute & validation integer expressions

--- src/telran/text/Strings.java
+++ src/telran/text/Strings.java
@@ -1,9 +1,20 @@
 package telran.text;
 
+import java.util.HashMap;
+import java.util.function.BinaryOperator;
+
 public class Strings {
+	static HashMap<String, BinaryOperator<Integer>> mapOperations;
+	static {
+		mapOperations = new HashMap<>();
+		mapOperations.put("-", (a, b) -> a - b);
+		mapOperations.put("+", (a, b) -> a + b);
+		mapOperations.put("*", (a, b) -> a * b);
+		mapOperations.put("/", (a, b) -> a / b);
+	}
 public static String javaVariableName() {
 	
-	return "[a-zA-Z$][\\w$]*|_[\\w$]+";
+	return "([a-zA-Z$][\\w$]*|_[\\w$]+)";
 }
 public static String zero_300() {
 	
@@ -12,11 +23,50 @@ public static String zero_300() {
 public static String ipV4Octet() {
 	//TODO
 	//positive number from 0 to 255 and leading zeros are allowed
-	return  "[01]?\\d\\d?|2([0-4]\\d|5[0-5])";
+	return  "([01]?\\d\\d?|2([0-4]\\d|5[0-5]))";
 }
 public static String ipV4() {
 	String octetRegex = ipV4Octet();
-	return String.format("((%s)[.]){3}(%s)",octetRegex, octetRegex );
+	return String.format("(%s\\.){3}%1$s",octetRegex);
+}
+public static String arithmeticExpression() {
+	String operandRE = operand();
+	String operatorRE = operator();
+	return String.format("%1$s(%2$s%1$s)*",operandRE, operatorRE );
+}
+public static String operator() {
+	return "\\s*([-+*/])\\s*";
+}
+public static String operand() {
+	//assumption: not unary operators
+	return "(\\d+)";
+}
+public static boolean isArithmeticExpression(String expression) {
+	expression = expression.trim();
+	return expression.matches(arithmeticExpression());
+}
+public static int computeExpression(String expression) {
+	
+	if (!isArithmeticExpression(expression)) {
+		throw new IllegalArgumentException("Wrong arithmetic expression");
+	}
+	expression = expression.replaceAll("\\s+", "");
+	String[] operands = expression.split(operator());
+	String [] operators = expression.split(operand());
+	int res = Integer.parseInt(operands[0]);
+	for(int i = 1; i < operands.length; i++) {
+		int operand = Integer.parseInt(operands[i]);
+		res = mapOperations.get(operators[i]).apply(res, operand);
+	}
+	
+	return res;
+}
+//Update whole code for any numbers (double)
+//Update code taking into consideration possible variable names
+public static double computeExpression(String expression,
+		HashMap<String, Double> mapVariables) {
+	//TODO
+	return 0;
 }
 
 }
